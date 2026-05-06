ce0cafabace915e24904c65003494becf663fab4
Satyam gupta <73402131+satyam9721@users.noreply.github.com>
2026-05-06 16:05:57 +0530

    Update Learn.js

--- Learn.js
+++ Learn.js
@@ -4,3 +4,18 @@
 > ls
 > cd nestjs-app
 > nest new project-name
+
+to run this project:-
+
+satyam_007@Satyams-MacBook-Air project-name % npm run build
+
+> project-name@0.0.1 build
+> nest build
+
+satyam_007@Satyams-MacBook-Air project-name % npm run start
+
+> project-name@0.0.1 start
+> nest start
+
+nestjs vs express.js
+
